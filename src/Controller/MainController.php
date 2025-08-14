55a8e2f678801bfa7544c8512d7a715f7f4f27c2
pierre <pi.senard974@gmail.com>
2025-08-14 10:17:39 +0200

    TP Bucket-list

---
+++ src/Controller/MainController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Repository\WishRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class MainController extends AbstractController
+{
+
+    #[Route('/', name: 'app_index', methods: ['GET'])]
+    public function index(): Response
+    {
+
+        return $this->render('main/index.html.twig');
+
+    }
+
+//    #[Route('/home', name: 'app_home')]
+//    public function home(): Response
+//    {
+//
+//        return $this->render('main/home2.html.twig');
+//    }
+
+
+    #[Route('/home/aboutUs', name: 'app_aboutUs')]
+    public function aboutUs(): Response
+    {
+
+        return $this->render('main/aboutUs.html.twig');
+    }
+
+    #[Route('/list', name: 'app_list')]
+    public function list(): Response
+    {
+
+
+        return $this->render('main/list.html.twig');
+    }
+
+
+    #[Route('/list/detail', name: 'app_detail')]
+    public function detail(WishRepository $wishRepository): Response
+    {
+        $wishes = $wishRepository->findAll();
+
+        return $this->render('main/detail.html.twig', [
+
+            'wishes' => $wishes,
+        ]);
+    }
+
+
+}
---
+++ src/Controller/WishController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Repository\WishRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class WishController extends AbstractController
+{
+    #[Route('/wish', name: 'wish')]
+    public function index(EntityManagerInterface $em ): Response
+    {
+        
+
+        return $this->render('wish/index.html.twig', [
+            'controller_name' => 'WishController',
+        ]);
+    }
+
+}
